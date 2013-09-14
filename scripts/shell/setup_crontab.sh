42c4823239a95005adbd318b676fbc2e4efbcad0
meteotester <dev@meteotester.com>
2013-09-14 14:19:36 +0200

    Updated stats.html and minor changes

--- scripts/shell/setup_crontab.sh
+++ scripts/shell/setup_crontab.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 cat <(crontab -l) <(echo "01 12 * * * wget http://www.meteotester.com/observations?place=Madrid >> meteotester_cron.log 2>&1
 02 12 * * * wget http://www.meteotester.com/forecasts?place=Madrid >> meteotester_cron.log 2>&1
-03 12 * * * wget 'http://www.meteotester.com/observations?place=Atlantic%20City' >> meteotester_cron.log 2>&1
-04 12 * * * wget 'http://www.meteotester.com/forecasts?place=Atlantic%20City' >> meteotester_cron.log 2>&1
+03 12 * * * wget 'http://www.meteotester.com/observations?place=Atlantic City' >> meteotester_cron.log 2>&1
+04 12 * * * wget 'http://www.meteotester.com/forecasts?place=Atlantic City' >> meteotester_cron.log 2>&1
 05 12 * * * wget http://www.meteotester.com/observations?place=Macau >> meteotester_cron.log 2>&1
 06 12 * * * wget http://www.meteotester.com/forecasts?place=Macau >> meteotester_cron.log 2>&1") | crontab -
 
--- src/main/java/com/meteotester/util/Parser.java
+++ src/main/java/com/meteotester/util/Parser.java
@@ -176,7 +176,6 @@ public class Parser {
 
 		}
 
-		//result += forecast;
 		return result;		
 
 	}
