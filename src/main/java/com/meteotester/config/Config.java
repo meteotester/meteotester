0ffa7d18d5b5fa96698a4b88d2c7a4d6002ca235
meteotester <dev@meteotester.com>
2014-07-15 18:03:13 +0200

    Added dummy Config.java

---
+++ src/main/java/com/meteotester/config/Config.java
@@ -0,0 +1,18 @@
+package com.meteotester.config;
+
+public class Config {
+
+	public static final String FORECASTIO_KEY = "{your_forecastio_key}";
+	public static final String OPENWEATHER_KEY = "{your_openweather_key}";
+	public static final String WUNDERGROUND_KEY = "{your_wunderground_key}";
+	public static final String WORLDWEATHERONL_KEY = "{your_worldweatheronl_key}";
+	
+	public static final int NUM_DAYS_WEB = 5;
+	public static final int NUM_DAYS_STORE = 5;
+	
+	public static final String S3_BUCKETNAME = "{your_s3_bucketname}";
+	public static final String AWS_ACCESS_KEY = "{your_aws_access_key}";
+	public static final String AWS_SECRET_KEY = "{your_aws_secret_key}";
+	
+	public static final String GEONAMES_ID = "{your_geonames_id}";
+}
